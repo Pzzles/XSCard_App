389cea8bdec6314d0c5ff6eed7e13bca5050df73
xolisa-liwani <xolisa@xspark.co.za>
2025-01-16 13:14:50 +0200

    i made a change to text inside view

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Luh Dzicky</Text>
+      <Text>Luuuuh yumy</Text>
       <StatusBar style="auto" />
     </View>
   );
