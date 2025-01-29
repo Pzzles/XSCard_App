3ca3c287d0b43c93203acfaf3b8bcba10f822caa
Pzzles <tshehlap@gmail.com>
2025-01-29 09:46:21 +0200

    navColor Done

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialIcons } from '@expo/vector-icons';
 import { COLORS } from '../constants/colors';
@@ -8,16 +8,54 @@ import { RootTabParamList } from '../types';
 import { createStackNavigator } from '@react-navigation/stack';
 import AddCards from '../screens/cards/AddCards';
 import EditCard from '../screens/contacts/EditCard';
+import { API_BASE_URL, ENDPOINTS, buildUrl } from '../utils/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
 const Stack = createStackNavigator();
 
 function TabNavigator() {
+  const [activeColor, setActiveColor] = useState(COLORS.secondary);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      loadUserColor();
+    }, [])
+  );
+
+  const loadUserColor = async () => {
+    try {
+      const storedUserData = await AsyncStorage.getItem('userData');
+      if (storedUserData) {
+        const parsedUserData = JSON.parse(storedUserData);
+        
+        // Fetch user details to get the color scheme
+        const response = await fetch(buildUrl(ENDPOINTS.GET_USER) + `/${parsedUserData.id}`);
+        const userData = await response.json();
+        
+        // Set color from user data
+        if (userData.colorScheme) {
+          setActiveColor(userData.colorScheme);
+        }
+      }
+    } catch (error) {
+      console.error('Error loading user color:', error);
+    }
+  };
+
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarActiveTintColor: COLORS.secondary,
+        tabBarActiveTintColor: activeColor,
         tabBarInactiveTintColor: COLORS.gray,
+        tabBarStyle: {
+          backgroundColor: COLORS.white,
+          borderTopWidth: 1,
+          borderTopColor: COLORS.gray + '20',
+          height: 60,
+          paddingBottom: 5,
+        },
         headerShown: false,
       }}
     >
