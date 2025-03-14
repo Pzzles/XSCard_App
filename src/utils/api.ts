783dbb7f504e94589ecf6cad446c16df14b8ef4c
The-A-Team-dot <moserhellshock@gmail.com>
2025-03-14 05:57:15 +0200

    Failed to load

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -24,7 +24,7 @@ const getBaseUrl = () => {
 
     if (__DEV__) {
       //  return 'http://192.168.2.242:8383';
-      return 'https://e2e4-41-114-75-200.ngrok-free.app';
+      return 'https://ac1b-41-114-75-200.ngrok-free.app';
     }
 
     return 'https://xscard-app.onrender.com';
