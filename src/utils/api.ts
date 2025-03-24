af6936857f40f987d579080b74614739429117f0
Pzzles <125101956+Pzzles@users.noreply.github.com>
2025-03-24 16:57:11 +0200

    test deployment

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -12,22 +12,22 @@ export interface PasscreatorResponse {
 }
 
 // Helper function to get the appropriate base URL
-// const getBaseUrl = () => {
-
-//   // return 'https://xscard-app.onrender.com';
-//     return 'http://192.168.2.237:8383';
-
-// };
-
 const getBaseUrl = () => {
-  console.log('Is Development Mode:', __DEV__);
 
-    if (__DEV__) {
-        return 'http://192.168.8.166:8383';
-    }
+   return 'https://xscard-app.onrender.com';
+    // return 'http://192.168.2.237:8383';
 
-    return 'https://xscard-app.onrender.com';
 };
+
+// const getBaseUrl = () => {
+//   console.log('Is Development Mode:', __DEV__);
+
+//     if (__DEV__) {
+//         return 'http://192.168.8.166:8383';
+//     }
+
+//     return 'https://xscard-app.onrender.com';
+// };
 
 
 export const API_BASE_URL = getBaseUrl();
