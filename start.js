e3cf71e5dd4144489870035892b3baa682afd4c9
Pzzles <tshehlap@gmail.com>
2025-08-20 15:21:32 +0200

    d

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8384;
+const PORT = process.env.PORT || 8383;
 
 let serverProcess = null;
 let expoProcess = null;
